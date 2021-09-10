57258bba9a5585285db18336846fa22552eea0f1
sergei-butko <s.butko-2026@kpi.ua>
2021-09-10 21:07:13 +0300

    3 - Create UI to show top artists

--- Portal_Application/ClientApp/src/app/home/home.component.ts
+++ Portal_Application/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Artist} from "./artist"
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
+
 export class HomeComponent {
+  artists: Artist[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Artist[]>(baseUrl + 'artist/top_artists').subscribe(result => {
+      this.artists = result;
+    }, error => console.error(error));
+  }
 }
