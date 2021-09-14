6136667c86ece41fa5b169201e4344e151e562ce
sergei-butko <s.butko-2026@kpi.ua>
2021-09-14 19:23:31 +0300

    3 - Create UI to show top artists (final changes)

--- Portal_Application/ClientApp/src/app/home/home.component.ts
+++ Portal_Application/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {AfterViewInit, Component, Inject} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Artist} from "./artist"
 
@@ -14,5 +14,12 @@ export class HomeComponent {
     http.get<Artist[]>(baseUrl + 'artist/top_artists').subscribe(result => {
       this.artists = result;
     }, error => console.error(error));
+  }
+
+  setColor(): string {
+    let colors: string[] = ["cadetblue", "coral", "cornflowerblue", "darkkhaki", "darkcyan", "lightgreen",
+      "darkseagreen", "plum", "gray", "lightsalmon", "lightskyblue", "orchid", "thistle"];
+    let randNum = Math.floor(Math.random() * colors.length);
+    return colors[randNum];
   }
 }
